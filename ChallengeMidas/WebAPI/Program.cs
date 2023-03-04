1e8b37139aa4c610391d018332d9b5792d2f9d2b
santiMoreno1989 <santiagom@gmail.com>
2023-03-03 23:15:37 -0300

    se configuraron los servicios y el connection strings

--- ChallengeMidas/WebAPI/Program.cs
+++ ChallengeMidas/WebAPI/Program.cs
@@ -1,6 +1,11 @@
+using Application;
+using Infraestructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfraestructureLayer(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
