7732f914da501861ac253b506f823760558d2c4f
santiMoreno1989 <santiagom@gmail.com>
2023-03-03 23:11:11 -0300

    configuracion de dependencias para los repositorios de hotel app

---
+++ ChallengeMidas/Infraestructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Infraestructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructureLayer(this IServiceCollection services,IConfiguration configuration) 
+        {
+            services.AddDbContext<HotelDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("HotelConnection"), optionsBuilder =>
+                    optionsBuilder.MigrationsAssembly(typeof(HotelDbContext).Assembly.FullName)), ServiceLifetime.Transient);
+
+            services.Scan(selector =>
+                selector.FromAssemblies(Assembly.GetExecutingAssembly())
+                    .AddClasses(filter=> filter.Where(type=> type.Name.EndsWith("Repository")))
+                    .AsImplementedInterfaces()
+                    .WithScopedLifetime()
+                    );
+
+            return services;
+        }
+    }
+}
