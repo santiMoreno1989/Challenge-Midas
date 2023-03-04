946e62fce5978bfecffc54ab45d4e81f53c30841
santiMoreno1989 <santiagom@gmail.com>
2023-03-03 23:09:24 -0300

    creacion de enum para tipo de habitaciones del hotel app

---
+++ ChallengeMidas/Domain/Enums/E_Habitacion.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Domain.Enums
+{
+    public enum E_Habitacion
+    {
+        [Description("Habitación doble clásica")]
+        DobleClasica = 1,
+
+        [Description("Habitación triple clásica")]
+        TripleClasica,
+
+        [Description("Habitación deluxe con cama extragrande")]
+        HabitacionDeluxe,
+
+        [Description("Suite con vistas a la montaña")]
+        Suite
+    }
+}
