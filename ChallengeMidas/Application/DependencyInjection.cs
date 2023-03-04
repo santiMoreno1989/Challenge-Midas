9609cf8bf020c9451ce6ac0ce75abcbd69dd48ee
santiMoreno1989 <santiagom@gmail.com>
2023-03-03 23:09:57 -0300

    configuracion de dependencias para los servicios

---
+++ ChallengeMidas/Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services) 
+        {
+            services.Scan(selector =>
+                selector.FromAssemblies(Assembly.GetExecutingAssembly())
+                .AddClasses(filter => filter.Where(type => type.Name.EndsWith("Service")))
+                .UsingRegistrationStrategy(RegistrationStrategy.Skip) // Permite agregar otros servicios con otro tipo de instancia
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+                );
+        
+            return services;
+        }
+    }
+}
