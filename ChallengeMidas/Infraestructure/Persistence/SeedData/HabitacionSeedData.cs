9ebb34f82dd1f69ff4032d8d1b8c6db6d0bb5b40
santiMoreno1989 <santiagom@gmail.com>
2023-03-03 23:13:47 -0300

    se cargan datos iniciales para las entidades habitacion y recepcionista

---
+++ ChallengeMidas/Infraestructure/Persistence/SeedData/HabitacionSeedData.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Persistence.SeedData
+{
+    public static class HabitacionSeedData
+    {
+        public static  void HabitacionSeed(this ModelBuilder builder) 
+        {
+            builder.Entity<Habitacion>().HasData(
+                new Habitacion 
+                {
+                    Id=1,
+                    TipoHabitacionEnum=(int)E_Habitacion.DobleClasica,
+                    Capacidad=2,
+                    Precio=10.500m
+                },
+                new Habitacion 
+                {
+                    Id=2,
+                    TipoHabitacionEnum=(int)E_Habitacion.TripleClasica,
+                    Capacidad=3,
+                    Precio=14.900m,
+                },
+                new Habitacion 
+                {
+                    Id=3,
+                    TipoHabitacionEnum=(int)E_Habitacion.HabitacionDeluxe,
+                    Capacidad=2,
+                    Precio=18.900m
+                },
+                new Habitacion 
+                {
+                    Id=4,
+                    TipoHabitacionEnum=(int)E_Habitacion.Suite,
+                    Capacidad=3,
+                    Precio=26.500m
+                });
+        }
+    }
+}
