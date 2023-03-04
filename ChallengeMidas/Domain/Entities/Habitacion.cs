a69b06cb69891f29ed58a02f392abf304e740fb8
santiMoreno1989 <santiagom@gmail.com>
2023-03-03 23:07:30 -0300

    Creacion de entidades

---
+++ ChallengeMidas/Domain/Entities/Habitacion.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class Habitacion
+    {
+        public int Id { get; set; }
+        public int TipoHabitacionEnum { get; set; }
+        public ushort Capacidad { get; set; }
+        public uint Precio { get; set; }
+        public bool? Disponible { get; set; }
+        public virtual ICollection<Reserva> Reservas { get; set; }
+        public virtual ICollection<Huesped> Huespedes { get; set; }
+    }
+}
