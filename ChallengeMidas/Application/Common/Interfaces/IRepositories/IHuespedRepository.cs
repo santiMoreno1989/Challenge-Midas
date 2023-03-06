b256b71f0c606dc1cb49fd7d7152bf9faa4ce5ab
santiMoreno1989 <santiagom@gmail.com>
2023-03-05 23:22:17 -0300

    Implementacion de Repository Pattern e Interfaces

---
+++ ChallengeMidas/Application/Common/Interfaces/IRepositories/IHuespedRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Common.Interfaces.IRepositories
+{
+    public interface IHuespedRepository : IBaseRepository<Huesped> 
+    {
+
+    }
+}
---
+++ ChallengeMidas/Infraestructure/Persistence/Repositories/HuespedRepository.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces.IRepositories;
+using Domain.Entities;
+
+namespace Infraestructure.Persistence.Repositories
+{
+    public class HuespedRepository : BaseRepository<Huesped>, IHuespedRepository
+    {
+        public HuespedRepository(HotelDbContext hotelDbContext) : base(hotelDbContext) { }
+
+    }
+}
