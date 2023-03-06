99d210184f8a9a55600a1865912da21183765dd2
santiMoreno1989 <santiagom@gmail.com>
2023-03-05 23:23:48 -0300

    Creacion de Dtos para Huesped entity

---
+++ ChallengeMidas/Application/Common/Dtos/Requests/HuespedRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.Dtos.Requests
+{
+    public class HuespedRequest
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+    }
+}
