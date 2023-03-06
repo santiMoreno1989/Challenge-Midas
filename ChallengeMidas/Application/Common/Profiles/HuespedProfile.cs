5224b96f48496db9008c51619cde53ed0bbe91d2
santiMoreno1989 <santiagom@gmail.com>
2023-03-05 23:26:04 -0300

    Implementacion de Automapper y creacion de Profle para Huesped entity

---
+++ ChallengeMidas/Application/Common/Profiles/HuespedProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Dtos.Requests;
+using Application.Common.Dtos.Responses;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Profiles
+{
+    public class HuespedProfile : Profile
+    {
+        public HuespedProfile()
+        {
+            CreateMap<HuespedRequest,Huesped>();
+            CreateMap<Huesped, HuespedResponse>();
+        }
+    }
+}
--- ChallengeMidas/WebAPI/Program.cs
+++ ChallengeMidas/WebAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationLayer();
 builder.Services.AddInfraestructureLayer(builder.Configuration);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
